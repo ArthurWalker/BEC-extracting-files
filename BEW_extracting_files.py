7b9106c0a0796517e9612c1e1ff771c4dd8720e1
ArthurWalker <phuc.phamhuu2@dcu.ie>
2019-03-15 16:13:35 +0000

    Evaluation not merge

--- BEW_extracting_files.py
+++ BEW_extracting_files.py
@@ -18,8 +18,13 @@ import xlrd
 path = os.path.join('C:/Users/pphuc/Desktop/Docs/Current Using Docs/')
 
 def write_file(path,folder_name,df,new_file_name):
+    empty_list = df[df.iloc[:,1]==''].index.tolist()
+    if (len(empty_list) > 0):
+        df = (df.drop(empty_list, axis=0).reset_index(drop=True))
     # Create a shared folder along side with year
     new_path = path +'Shared Data/'
+    if re.search('Evaluations',new_path):
+        new_path = re.sub(r'BEW 2012/Evaluations/','',new_path)
     if not os.path.exists(new_path):
         os.makedirs(new_path)
         if not (os.path.isfile(new_path + new_file_name+'.xlsx')):
@@ -35,14 +40,8 @@ def write_file(path,folder_name,df,new_file_name):
             df.iloc[1:, :].to_excel(writer, new_file_name, index=False, header=False,startrow=writer.sheets[new_file_name].max_row)
             writer.save()
 
-def write_to_1_file(path,df):
-    empty_list = df[df[1] == ''].index.tolist()
-    if (len(empty_list) > 0):
-        df = (df.drop(empty_list, axis=0).reset_index(drop=True))
-    write_file(path,'',df,'Evaluation')
 
-
-def extract_data(excel_file,tab,extracted_lst,skiprow):
+def extract_data(excel_file,tab,extracted_lst,skiprow,project_year):
     temp_df = pd.read_excel(excel_file, tab, skiprows=skiprow,keep_default_na=False, header=None)
     print('Phase 2')
     df_1_line = temp_df.iloc[0]
@@ -50,11 +49,15 @@ def extract_data(excel_file,tab,extracted_lst,skiprow):
     if tab == 'Technologies':
         print('Phase 4')
         extracted_df = temp_df.iloc[:,2:]
+        extracted_df.insert(0, '',project_year)
+        extracted_df.iloc[0, 0] = 'Year'
     else:
         col_index_workbook = find_column(df_1_line, extracted_lst)
         col_extended_workbook = find_extended_column(tab, df_1_line, col_index_workbook)
         print('Phase 4')
         extracted_df = temp_df[col_extended_workbook]
+        extracted_df.insert(0, '',project_year)
+        extracted_df.iloc[0, 0] = 'Year'
     return extracted_df
 
 def find_extended_column(tab,df_l_line,num_list):
@@ -77,15 +80,15 @@ def find_column(df_1_line,lst_to_find):
 
 def assign_task_Evaluation(seeep_path,folder):
     input_folder = seeep_path + folder + '/'
+    project_year = '2012'
     file_path_lst = os.listdir(input_folder)
     print (file_path_lst)
     for file in file_path_lst:
         if re.search(r'Batch',file):
             excel_file = pd.ExcelFile(input_folder+file)
             col_lst = ['Reference', 'Applicant', 'Description']
-            summary_df = extract_data(excel_file,'Summary Sheet',col_lst,0)
-            write_to_1_file(seeep_path[:-9],summary_df)
-
+            summary_df = extract_data(excel_file,'Summary Sheet',col_lst,0,project_year)
+            write_file(input_folder,'',summary_df[:-9],'Summary')
 
 def assign_task_Summary(seeep_path,file,folder):
     input_folder = seeep_path + folder + '/'
@@ -94,7 +97,7 @@ def assign_task_Summary(seeep_path,file,folder):
     excel_file = pd.ExcelFile(input_folder + file)
     if 'Admin' in excel_file.sheet_names:
         lst_col_admin = ['Reference No.','Cat. ','Submitted By','Project Title','County','Approved Funding']
-        admin_df = extract_data(excel_file,'Admin',lst_col_admin,1)
+        admin_df = extract_data(excel_file,'Admin',lst_col_admin,1,project_year)
         write_file(seeep_path,folder,admin_df,'Admin')
 
 def assign_task_Overview(seeep_path,file,folder):
@@ -104,26 +107,26 @@ def assign_task_Overview(seeep_path,file,folder):
     print ('Phase 0')
     excel_file = pd.ExcelFile(input_folder + file)
     print ('Phase 1')
-    if 'BE Workplaces main workbook' in excel_file.sheet_names:
-        lst_col_workbook = ['SEAI Reference', 'Organisation', 'Project Title', 'Total Incl VAT', 'Total Excl VAT',
-                            'Select Thermal Fuel', 'Total Energy Cost Savings €', 'Grant  /Approved (Proposed)',
-                            'Grant /Approved (Proposed)', 'Primary Energy Savings kWh', 'Site Energy Reduction %']
-        df_workplaces = extract_data(excel_file,'BE Workplaces main workbook',lst_col_workbook,3)
-        write_file(seeep_path,folder, df_workplaces,'Workplaces')
-    # if 'Technologies' in excel_file.sheet_names:
-    #     lst_col_tech = []
-    #     tech_df = extract_data(excel_file,'Technologies',lst_col_tech,0)
-    #     write_file(seeep_path,folder, tech_df,'Technologies')
-    # print ('Phase 5')
+    # if 'BE Workplaces main workbook' in excel_file.sheet_names:
+    #     lst_col_workbook = ['SEAI Reference', 'Organisation', 'Project Title', 'Total Incl VAT', 'Total Excl VAT',
+    #                         'Select Thermal Fuel', 'Total Energy Cost Savings €', 'Grant  /Approved (Proposed)',
+    #                         'Grant /Approved (Proposed)', 'Primary Energy Savings kWh', 'Site Energy Reduction %']
+    #     df_workplaces = extract_data(excel_file,'BE Workplaces main workbook',lst_col_workbook,3,project_year)
+    #     write_file(seeep_path,folder, df_workplaces,'Workplaces')
+    if 'Technologies' in excel_file.sheet_names:
+        lst_col_tech = []
+        tech_df = extract_data(excel_file,'Technologies',lst_col_tech,0,project_year)
+        write_file(seeep_path,folder, tech_df,'Technologies')
+    print ('Phase 5')
 
 def execute_each_folder(seeep_path,folder_name):
     file_path = seeep_path+folder_name+'/'
     file_path_lst = os.listdir(file_path)
     for file in file_path_lst:
-        # if file == 'Evaluations':
-        #     assign_task_Evaluation(seeep_path+'BEW 2012/',file)
-        # if re.search(r'Better Energy',file) and re.search(r'Summary',file):
-        #     assign_task_Summary(seeep_path,file,folder_name)
+        if file == 'Evaluations':
+            assign_task_Evaluation(seeep_path+'BEW 2012/',file)
+        if re.search(r'Better Energy',file) and re.search(r'Summary',file):
+            assign_task_Summary(seeep_path,file,folder_name)
         if re.search(r'Better Energy Board',file):
             assign_task_Overview(seeep_path,file,folder_name)
 
